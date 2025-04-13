9fc8a94821ee3ac739510e8fc277c79305e548cb
bimengzhou <dylanmengzhou@gmail.com>
2025-04-14 02:11:08 +0900

    version 1.1.0: 添加登录功能

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const LoginPage = () => {
 
